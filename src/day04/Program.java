95e0f69aa3fe0afdbeb257c50af051404badc883
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-02 08:43:21 +0300

    Snn has been changed

---
+++ src/day04/Program.java
@@ -0,0 +1,11 @@
+package day04;
+
+public class Program {
+    public static void main(String[] args) {
+        // Task: Yıldızlardan oluşan bir kare çıktısı veren bir java programı yazınız.
+
+        System.out.println("*******");
+        System.out.println("*******");
+        System.out.println("*******");
+    }
+}

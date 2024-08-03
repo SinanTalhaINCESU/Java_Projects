3c841c78f1a9d32d402a88da7cd996c77bf0af42
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-03 20:30:06 +0300

    Snn has been changed

---
+++ src/day08/Summary.java
@@ -0,0 +1,38 @@
+package day08;
+
+public class Summary {
+    public static void main(String[] args) {
+        System.out.println("Merhaba!");
+
+        int sayi=5;
+
+        char c='A';
+
+        double kutununAgirligi=3.0;
+
+        float f=3.0F;
+
+
+        String adi="Ahmet";
+        String soyadi="Dursun";
+
+        String isim=adi+" "+soyadi;
+
+        System.out.println(isim);
+
+
+        boolean result1=true;
+        boolean result2=false;
+        boolean result3=6<3;
+
+        String str="23"+18;
+
+        System.out.println(str);
+
+
+
+        String mesaj="Çocuklar, hep bir ağızdan \"Neden biz susmak zorundayız?\" dediler";
+        System.out.println(mesaj);
+
+    }
+}

5f3d9f2a0240972a0ec3e00c8e87c303fe24f5d3
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-03 20:29:26 +0300

    Snn has been changed

---
+++ src/day06/VariableDeclaration2.java
@@ -0,0 +1,12 @@
+package day06;
+
+public class VariableDeclaration2 {
+    public static void main(String[] args) {
+        // int max ve min değerlerini yazdır.
+
+        int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
+
+        System.out.println("Integer max ve min değerleri = " +max +" , "+min);
+    }
+}
---
+++ src/day06/Variables.java
@@ -0,0 +1,11 @@
+package day06;
+
+public class Variables {
+    public static void main(String[] args) {
+        //karenin alanı ve çevresini hesaplayınız.
+
+        int kenar=5;
+        System.out.println(kenar*kenar);
+        System.out.println(kenar*4);
+    }
+}

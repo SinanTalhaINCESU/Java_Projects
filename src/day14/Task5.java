6647410a2d2707897c057ffd104e12e4abe4970b
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-06 12:38:50 +0300

    Snn has been changed

---
+++ src/day14/Task5.java
@@ -0,0 +1,15 @@
+package day14;
+
+public class Task5 {
+    public static void main(String[] args) {
+        /*
+            Eğer sıcaklık değişkeni 70'den büyük ve 80'den küçükse "Ideal Temp" yazdıran bir if ifadesi yazın.
+         */
+
+        int sicaklik=75;
+
+        if (sicaklik<80 & sicaklik>70){
+            System.out.println("Ideal Temp");
+        }
+    }
+}

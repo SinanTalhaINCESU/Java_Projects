2603cb07f86d7a640caa6b0a0e5c33160efc3b88
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-07 16:28:23 +0300

    Snn has been changed

---
+++ src/day15/Task4.java
@@ -0,0 +1,10 @@
+package day15;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int x=100, y=100;
+        String mesaj=x>y?"x büyüktür":x<y?"y büyüktür":"x ve y eşittir";
+        System.out.println(mesaj);
+    }
+}

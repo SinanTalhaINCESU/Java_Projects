941f1aaff5de7ea16530bddda274eb6a00335f5c
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-12 16:53:45 +0300

    Snn has been changed

---
+++ src/day18/Task.java
@@ -0,0 +1,21 @@
+package day18;
+
+public class Task {
+    /*
+            Klavyeden veri okumak üzere aşağıda istenenleri gerçekleştiriniz.
+            1. Matematik işlemleri
+            2. Alan ve Çevre Hesaplama
+            3. Çıkış
+
+            Alt Menü | Matematik İşlemleri
+            1. Faktöriyel Hesaplama
+            2. Mutlak Değer Alma
+            3. Çıkış
+
+            Alt Menü | Alan ve Çevre Hesaplama
+            1. Dikdörtgen
+            2. Kare
+            3. Daire
+            4. Çıkış
+         */
+}

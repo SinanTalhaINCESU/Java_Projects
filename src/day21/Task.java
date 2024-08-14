b5c9416afc1ad80adf940c89cc0344a064e940ae
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-14 16:52:07 +0300

    Snn has been changed

---
+++ src/day21/Task.java
@@ -0,0 +1,17 @@
+package day21;
+
+public class Task {
+    public static void main(String[] args) {
+
+        //String[ ] data={"Ahmet","Fizik","Şengül","Kimya","Hakan","Matematik","Feraye","Biyoloji","Cengiz","Felsefe","Salih",
+        // "Elektronik","Sezen","Bilgisayar","Hasan","Fizik","Sezgin","Biyoloji" };
+        //
+        //Yukarıdaki dizi verildiğine göre;
+        //Adı Bölümü
+        //Ahmet Fizik
+        //
+        //biçiminde ekran çıktısı veren bir Java programı yazınız.
+
+
+    }
+}

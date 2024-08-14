c9b240ec55906886c8c7f171d978a35aafb60918
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-14 16:52:38 +0300

    Snn has been changed

--- src/day21/Task.java
+++ src/day21/Task.java
@@ -12,6 +12,20 @@ public class Task {
         //
         //biçiminde ekran çıktısı veren bir Java programı yazınız.
 
-
+        String[] data={"Ahmet","Fizik","Şengül","Kimya","Hakan","Matematik","Feraye","Biyoloji","Cengiz",
+                "Felsefe","Salih","Elektronik","Sezen","Bilgisayar","Hasan","Fizik","Sezgin","Biyoloji" };
+        String arananKisi="Ahmet";
+        String arananDers="Fizik";
+        boolean bulundu = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(arananKisi) & data[i+1].equals(arananDers)){
+                System.out.println("Adı     Bölümü");
+                System.out.println(data[i]+"   "+data[i+1]);
+                bulundu = true; break;
+            }
+        }
+        if (!bulundu){
+            System.out.println("Aranan kişi ve ders bulunamadı.");
+        }
     }
 }

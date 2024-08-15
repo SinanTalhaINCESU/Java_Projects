c6bf356500b6d057a00c670c5ebf071636681386
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-15 23:13:09 +0300

    Snn has been changed

--- src/day22/Words.java
+++ src/day22/Words.java
@@ -16,6 +16,21 @@ public class Words {
         //    System.out.println(input.next());
         //}
 
+        String cumle="i like the way you die, boy.";
+        String[] kelimeler=cumle.split(" ");
+        for (String kelime:kelimeler) {
+            System.out.println(kelime);
+        }
+        System.out.println();
 
+        for (int i = 0; i < kelimeler.length; i++) {
+            System.out.println(kelimeler[i]);
+        }
+        System.out.println();
+
+        int counter=0;
+        while (counter< kelimeler.length){
+            System.out.println(kelimeler[counter++]);
+        }
     }
 }

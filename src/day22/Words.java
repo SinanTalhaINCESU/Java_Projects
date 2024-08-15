040250708cbe3451350166697a19eae7072146b4
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-15 23:12:51 +0300

    Snn has been changed

---
+++ src/day22/Words.java
@@ -0,0 +1,21 @@
+package day22;
+
+public class Words {
+    public static void main(String[] args) {
+
+        //System.out.print("Cümle : ");
+        //Scanner input=new Scanner(System.in);
+        //while (input.hasNext()){
+        //    System.out.println(input.next());
+        //}
+
+
+        //String cumle="i like the way you die, boy.";
+        //Scanner input=new Scanner(cumle);
+        //while (input.hasNext()){
+        //    System.out.println(input.next());
+        //}
+
+
+    }
+}

5334c0444dd0a5310952242cf407fad3f89cadb4
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-18 23:45:29 +0300

    Snn has been changed

--- src/day24/Program.java
+++ src/day24/Program.java
@@ -3,5 +3,14 @@ package day24;
 public class Program {
     public static void main(String[] args) {
 
+        Kopek k1=new Kopek();
+
+        k1.cinsi="Kangal";
+        k1.yasi=3;
+        k1.agirligi=11.1;
+
+        k1.bilgiGoster();
+
+        System.out.println();
     }
 }

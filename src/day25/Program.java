cab5ae133d4f5e33a50db34ef185d3397b438660
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-19 18:13:48 +0300

    Snn has been changed

---
+++ src/day25/Program.java
@@ -0,0 +1,9 @@
+package day25;
+
+public class Program {
+    public static void main(String[] args) {
+        Kare kare1=new Kare();
+        kare1.kenar=7;
+        kare1.bilgiGoster();
+    }
+}

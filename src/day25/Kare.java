7f41ab36d40e751621f5bef7a2d8e07991f5ef8b
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-19 18:13:14 +0300

    Snn has been changed

--- src/day25/Kare.java
+++ src/day25/Kare.java
@@ -11,4 +11,9 @@ public class Kare {
     public int cevreHesapla(){
         return 4*kenar;
     }
+
+    public void bilgiGoster(){
+        System.out.printf("Alan : %d\n",alanHesapla());
+        System.out.printf("Cevre: %d\n",cevreHesapla());
+    }
 }

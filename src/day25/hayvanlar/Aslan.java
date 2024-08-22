ad9e4ee6e3e87fb1bb3e71ef8dcab445bac201ca
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-22 23:07:25 +0300

    Snn has been changed

--- src/day25/hayvanlar/Aslan.java
+++ src/day25/hayvanlar/Aslan.java
@@ -7,4 +7,8 @@ public class Aslan {
     public String renk;
     public double boy;
     public double kilo;
+
+    public void bilgiGoster(){
+        System.out.printf("%s aslanı, cinsi %s olup, %s rengindedir. Boyu %5.2f metre, kilosu %5.2f kg'dir.",tur,cins,renk,boy,kilo);
+    }
 }

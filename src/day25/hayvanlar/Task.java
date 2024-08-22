fefe95a428bd9c2403f4387680d868a3ba0be286
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-22 23:07:56 +0300

    Snn has been changed

---
+++ src/day25/hayvanlar/Task.java
@@ -0,0 +1,13 @@
+package day25.hayvanlar;
+
+public class Task {
+    public static void main(String[] args) {
+
+        Aslan aslan1=new Aslan();
+        aslan1.tur="Afrika Aslanı";
+        aslan1.cins="erkek";
+        aslan1.renk="koyu kahve";
+        aslan1.boy=2.50;
+        aslan1.kilo=250;
+    }
+}

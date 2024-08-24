5689bf979f904b8a809ac166724ef736ac0c622e
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-24 17:54:01 +0300

    Snn has been changed

---
+++ src/day26/DortIslem.java
@@ -0,0 +1,12 @@
+package day26;
+
+public class DortIslem {
+
+    public int sayi1;
+
+    public int sayi2;
+
+    public DortIslem(int sayi1,int sayi2){
+        this.sayi1=sayi1;
+        this.sayi2=sayi2;
+}

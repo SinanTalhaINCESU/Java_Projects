0541481a359b6e73ae2be3a848de356807ef6fee
SinanTalhaINCESU <snn99tlh@hotmail.com>
2024-08-24 17:55:45 +0300

    Snn has been changed

---
+++ src/day26/Program.java
@@ -0,0 +1,10 @@
+package day26;
+
+public class Program {
+    public static void main(String[] args) {
+
+        DortIslem islem1=new DortIslem(5,7);
+        System.out.println("Sayıların Toplamı = "+islem1.topla());
+        System.out.println("Sayıların Çarpımı = "+islem1.carp());
+    }
+}
